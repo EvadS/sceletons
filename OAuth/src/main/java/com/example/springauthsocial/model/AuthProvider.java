47b8b135d11ed45f73ec117a4d27f4f1f49770ad
Evgeniy Skiba <eskiba@s-pro.io>
2019-11-18 08:17:09 +0200

    Spring Boot OAuth2 Social Login with Google, Facebook, and Github - Part 1

---
+++ OAuth/src/main/java/com/example/springauthsocial/model/AuthProvider.java
@@ -0,0 +1,8 @@
+package com.example.springauthsocial.model;
+
+public enum  AuthProvider {
+    local,
+    facebook,
+    google,
+    github
+}
---
+++ OAuth/src/test/java/com/example/springauthsocial/SpringAuthSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springauthsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
